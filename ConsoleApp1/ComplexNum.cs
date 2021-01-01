d01691d6ca407f3d5331127cc4cf8ca4b94b6c49
Kayra Çakıroğlu <58420259+kairaa@users.noreply.github.com>
2021-01-01 03:28:22 +0300

    Simple Complex Number Calculator

---
+++ ConsoleApp1/ComplexNum.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ComplexNum
+    {
+        public double real;
+        public double imaginary;
+
+        public ComplexNum(double r, double i)
+        {
+            real = r;
+            imaginary = i;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(imaginary >= 0 ? real + "+" + imaginary + "i" : real + "" + imaginary + "i");
+        }
+
+        public static implicit operator ComplexNum(double sayi)
+        {
+            return new ComplexNum(sayi, 0);
+        }
+
+        public static explicit operator double (ComplexNum c)
+        {
+            return Math.Sqrt((c.real*c.real) + (c.imaginary*c.imaginary));
+        }
+
+        public static ComplexNum calculateConjugate(ComplexNum c)
+        {
+            return new ComplexNum(c.real, (-1)*c.imaginary);
+        }
+
+        public static ComplexNum operator+(ComplexNum c1,ComplexNum c2)
+        {
+            double r = c1.real + c2.real;
+            double i = c1.imaginary + c2.imaginary;
+            return new ComplexNum(r, i);
+        }
+
+        public static ComplexNum operator-(ComplexNum c1, ComplexNum c2)
+        {
+            double r = c1.real - c2.real;
+            double i = c1.imaginary - c2.imaginary;
+            return new ComplexNum(r, i);
+        }
+
+        public static ComplexNum operator*(ComplexNum c1, ComplexNum c2)
+        {
+            double r = (c1.real * c2.real)-(c2.imaginary*c1.imaginary);
+            double i = (c1.real * c2.imaginary) + (c1.imaginary*c2.real);
+            return new ComplexNum(r, i);
+        }
+
+        public static ComplexNum operator/(ComplexNum c1,ComplexNum c2)
+        {
+            double a = c1.real;
+            double b = c1.imaginary;
+            double c = c2.real;
+            double d = c2.imaginary;
+
+
+            double real = ((a * c) + (b * d)) / (c * c + d * d);
+            double imaginary = ((b * c) - (a * d)) / (c * c + d * d);
+
+            return new ComplexNum(real, imaginary);
+        }
+
+        public static bool operator==(ComplexNum c1,ComplexNum c2)
+        {
+            return c1.real == c2.real && c1.imaginary == c2.imaginary;
+        }
+
+        public static bool operator!=(ComplexNum c1, ComplexNum c2)
+        {
+            return c1.real != c2.real || c1.imaginary != c2.imaginary;
+            //return !(c1==c2)
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNum c1 = new ComplexNum(4, 3);
+            ComplexNum c2 = new ComplexNum(2, 3);
+            c1.print();
+            c2.print();
+
+            ComplexNum c3 = c1 + c2;
+            c3.print();
+
+            ComplexNum c4 = c1 - c2;
+            c4.print();
+
+            ComplexNum c5 = c1 * c2;
+            c5.print();
+
+            ComplexNum c6 = c1 / c2;    //doğru çalışmıyor
+            c6.print();
+
+            ComplexNum c8 = 8;          //implicit
+            c8.print();
+
+            Console.WriteLine(c1 == c2);
+
+            /*ComplexNum c9 = new ComplexNum(2, 3);
+            c9.birYaz(c9);*/            
+
+            double c10 = (double)c1;    //explicit
+            Console.WriteLine(c10);
+
+            ComplexNum c11 = c10;   //implicit (c1 den geleni X+0i biçimine döndürüyor)
+            c11.print();
+
+            Console.ReadLine();
+        }
+    }
+}
